6f21098d3498eeedef6162cd5b24a3d363d1922e
danielkellydev <danielkellydev@gmail.com>
2024-01-16 14:11:52 +1100

    added title, audience and outline input fields

--- app/controllers/openai_controller.rb
+++ app/controllers/openai_controller.rb
@@ -1,6 +1,7 @@
 class OpenaiController < ApplicationController 
   def index
-    service = OpenaiService.new(params[:query])
+    button = params[:help_me_get_started] != "" ? "help_me_get_started" : "analyse"
+    service = OpenaiService.new(params[:title], params[:audience], params[:outline], params[:query], button)
     @response = service.call
     render turbo_stream: turbo_stream.update('response', partial: 'response', locals: { response: @response })
   end
--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -2,8 +2,12 @@ class OpenaiService
   include HTTParty
   attr_reader :api_url, :options, :query 
 
-  def initialize(query)
+  def initialize(title, audience, outline, query, button)
+    @title = title 
+    @audience = audience
+    @outline = outline 
     @query = query 
+    @button = button 
     @api_url = 'https://api.openai.com/v1/chat/completions'
     @options = {
       headers: {
@@ -14,12 +18,24 @@ class OpenaiService
   end
 
   def call 
+    prompt = generate_prompt
     body = {
       model: 'gpt-4-1106-preview',
-      messages: [{ role: 'user', content: query }]
+      messages: [{ role: 'user', content: prompt }]
     }
     response = HTTParty.post(api_url, body: body.to_json, headers: options[:headers], timeout: 500)
     raise response['error']['message'] unless response.code == 200
     response['choices'][0]['message']['content']
+  end
+
+  private 
+
+  def generate_prompt 
+    if @button == "help_me_get_started"
+      "Given that your title is '#{@title}', your audience is '#{@audience}', and your outline is '#{@outline}', here are some suggestions to get you started:"
+    elsif @button == "analyse"
+      # Generate a prompt to analyse the user's writing
+      "Given that your title is '#{@title}', your audience is '#{@audience}', your outline is '#{@outline}', and you've written '#{@query}', here's an analysis of your writing:"
+    end
   end
 end
